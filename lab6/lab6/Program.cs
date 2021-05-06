879edf53bdd69a91dfef99acc3da3eef95488bd6
vadinamo <75493220+vadinamo@users.noreply.github.com>
2021-05-06 09:56:55 +0300

    Add files via upload

--- lab6/lab6/Program.cs
+++ lab6/lab6/Program.cs
@@ -11,39 +11,39 @@ namespace lab6
             List<Truck> truck = new List<Truck>();
             List<Motorcycle> motorcycle = new List<Motorcycle>();
             int pick = -1;
-            UserInterface Usr = new UserInterface();
+            UserInterface userInterface = new UserInterface();
             
             while (pick != 0)
             {
                 Console.WriteLine("1. View all vehicles.\n2. Add vehicle.\n3. Edit vehicle.\n" +
                                   "4. Delete vehicle.\n5. Vehicle information.\n0. Exit.");
 
-                pick = Usr.IntCheck();
+                pick = userInterface.IntCheck();
 
                 switch (pick)
                 {
                     case 1:
-                        Usr.ViewAllVehicles(car, truck, motorcycle);
+                        userInterface.ViewAllVehicles(car, truck, motorcycle);
 
                         break;
 
                     case 2:
-                        Usr.AddVehicle(car, truck, motorcycle);
+                        userInterface.AddVehicle(car, truck, motorcycle);
 
                         break;
 
                     case 3:
-                        Usr.EditVehicle(car, truck, motorcycle);
+                        userInterface.EditVehicle(car, truck, motorcycle);
 
                         break;
 
                     case 4:
-                        Usr.DeleteVehicle(car, truck, motorcycle);
+                        userInterface.DeleteVehicle(car, truck, motorcycle);
 
                         break;
 
                     case 5:
-                        Usr.VehicleInformation(car, truck, motorcycle);
+                        userInterface.VehicleInformation(car, truck, motorcycle);
 
                         break;
                 }
