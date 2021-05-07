3399c2907411dd0b2a59aa68b3297b40eefd9c71
vadinamo <75493220+vadinamo@users.noreply.github.com>
2021-05-07 15:00:51 +0300

    Add files via upload

--- lab6/lab6/IComparer.cs
+++ lab6/lab6/IComparer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace lab6
@@ -6,16 +5,7 @@ namespace lab6
     interface IComparer
     {
         int Compare(object o1, object o2);
-    }
-
-    class VehicleComparer : IComparer<Vehicle>
-    {
-        public int Compare(Vehicle a, Vehicle b)
-        {
-            if (a != null && b != null && a.Price < b.Price)
-                return 1;
-            else
-                return 0;
-        }
+        void SortByPrice(List<Vehicle> vehicle);
+        void Swap<T>(IList<T> list, int indexA, int indexB);
     }
 }
